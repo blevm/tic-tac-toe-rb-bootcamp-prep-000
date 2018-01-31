9ca0cec71219c9072288f39644a2676552a371be
Michelle B <mjblevins7@gmail.com>
2018-01-31 05:41:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,6 +53,26 @@ def play(board)
   end
 end
 
+def turn_count(board)
+  turn_counter = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+    turn_counter += 1
+    end
+  end
+  turn_counter
+end
+
+
+def current_player(board)
+  turn = turn_count(board)
+  if turn.even?
+    "X"
+  else
+    "O"
+  end
+end
+
 # Define your WIN_COMBINATIONS constant
 #WIN_COMBINATIONS = [
 #  [0,1,2],
